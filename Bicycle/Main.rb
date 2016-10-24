9e5cd748ac7d181c49d452e17a14d4d92fe49f47
facundobazzana <facundobazzana@gmail.com>
2016-10-24 00:28:23 -0300

    Segundo Commit

--- Bicycle/Main.rb
+++ Bicycle/Main.rb
@@ -2,10 +2,12 @@ require 'Gear.rb'
 require 'Wheel.rb'
 
 class Main
-
+  #Creo el objeto Gear
   puts Gear.new(:chainring => 52,
                 :cog => 11,
                 :wheel => Wheel.new(26,1.5)).gear_inches
+  
+  #Creo el objeto Gear
   puts Gear.new(:chainring => 52,
                 :cog => 11,
                 :wheel => Wheel.new(26,1.5)).ratio
